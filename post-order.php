fce451d9399063f49a9a79de9c72c453ee240564
Yonkov <yonkov.atanas@gmail.com>
2019-09-30 12:51:18 +0300

    first commit

---
+++ post-order.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Plugin Name: Post Order By Category
+ * Description: A lightweight plugin that adds the option to reverse the post order for a specific category to be date ascending. When creating or editing a category from the Admin Dashboard, the user can choose to sort the posts for that category by oldest or newest (default WordPress sort). Useful for journals, books or achives, who need to have a chronological sort order for certain category archive's pages.
+ * Version: 1.0
+ * Author: Atanas Yonkov
+ * Author URI: https://yonkov.github.io/
+ * Tags: post order, chronological, reverse post order
+ * License: GPL
+
+=====================================================================================
+Copyright (C) 2019 Atanas Yonkov
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with WordPress; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+=====================================================================================
+*/
+
+function changePostOrder(){
+    //get the category id from the admin url
+    $cat_id = $_GET['tag_ID'];
+    $post_order = get_term_meta($cat_id, 'post-order', true);
+    
+    ?>
+    
+    <tr class="form-field">
+        <th scope="row" valign="top"><label for="post-order"><?php _e('Post Order'); ?></label></th>
+        <td>
+            
+        <select name="post-order" id="post-order">
+            <option <?php if ($post_order == 'newest') { ?>selected="true" <?php }; ?>value="newest"><?php _e('Newest'); ?></option>
+            <option <?php if ($post_order == 'oldest') { ?>selected="true" <?php }; ?>value="oldest"><?php _e('Oldest'); ?></option>
+        </select>
+        </td>
+    </tr>
+    
+    <?php
+    
+}
+add_action ( 'edit_category_form_fields', 'changePostOrder');
+
+function saveCategoryFields($query) {
+    //get the category id from the admin url
+    $cat_id = $_POST['tag_ID'];
+    //check if the user is an admin or an editor
+    if(current_user_can( 'manage_categories' ) && isset( $_POST['post-order'] )){
+        if ($_POST['post-order'] == 'oldest')  {
+            //add meta value in wp_termmeta table
+            //update field
+            $order = sanitize_title( $_POST['post-order'] );
+            update_term_meta($cat_id, 'post-order', $order); 
+        }
+        else if ($_POST['post-order'] == 'newest')  {
+            //add meta value in wp_termmeta table
+            //update field
+            $order = sanitize_title( $_POST['post-order'] );
+            update_term_meta($cat_id, 'post-order', $order); 
+        }
+    }
+}
+add_action ( 'edited_category', 'saveCategoryFields');
+
+function simple_post_order_category_order( $query ) {
+    //get the category id when the archive page is being displayed
+    $category = get_queried_object();
+    $cat_id = $category->term_id;
+    //get the meta value
+    $post_order = get_term_meta($cat_id, 'post-order', true);
+    
+    if ( $post_order =='oldest' && $query->is_category($cat_id) && $query->is_main_query() ) {
+        $query->set( 'order', 'ASC' );
+    }
+}
+add_action( 'pre_get_posts', 'simple_post_order_category_order' );
---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== Post Order By Category ===
+Contributors: nravota12
+Tags: post order, chronological, reverse post order
+Requires at least: 4.4
+Tested up to: 5.2
+Stable tag: 1.0
+Description: A lightweight plugin that adds the option to reverse the post order for a specified category to be date ascending. When creating or editing a category from the Admin Dashboard, the user can choose to sort the posts for that category by oldest or newest (default WordPress sort). Useful for journals, books or achives, who need to have a chronological sort order for certain category archive's pages.
+License: GPLv2
+
+== Description ==
+Chronological posts reverses the default post order throughout your site to be chronological instead of reverse-chronological. This can be useful for sites displaying historical content, journals, books, etc. where the content is intended to be read chronologically (typically, it works well for any sites where the entire content of the site - every post - is intended to be read sequentially). It is also useful for archived sites, preserving the history of posts in a sequential format, oldest to newest.
+
+== Installation ==
+1. Take the easy route and install through the WordPress plugin installer, or,
+1. Download the .zip file and upload the unzipped folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+= Where are the options? =
+When editing or updating a category from the WordPress dashboard (click on posts => categories => edit), a dropdown to select options will appear after the category description. The options are "Oldest" (show posts by date ascending or the so called chronological order) or "Newest" (show posts by date ascending or the WordPress way). The default value is "Newest", which is the same as the WordPress default post sort.
+When you choose "Oldest", the plugin will automatically display the posts from that category archive page in chronological order. If you want to change the display order for other category archive pages, you can 
+
+= Displaying only specific post types in chronological order =
+The plugin is able to change the sort order to a specific post type. See the above question.
+
+= Display other sort options
+The plugin can be modified to sort posts by other criteria, including post title. You can also customize it to sort posts by more than one criteria. Feel free to contact me for more information on how to do it! The plugin does exactly what it states it does. If you like it, please give it a 5-star rating.
+== Changelog ==
+= 1.0 =
+* First publicly available version of the plugin.
+
+== Upgrade Notice ==
+= 1.0 =
+Initial release.
